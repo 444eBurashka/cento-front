06a0586d5cc41734d522458c52b05fc7f8308672
Emma <n.ba.04@mail.ru@>
2024-10-28 16:59:42 +0500

    создала ветку для отдельной работы и доделала макет авторизации

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,17 @@
 import Button from "./Button";
 import Input from "./Input";
 
-export default function Footer(props) {
+export default function Form(props) {
     return(
         <form className="Form">
             <h2>Личный кабинет</h2>
             <Input textLabel="Логин" placeholder="Введите почту или номер телефона"/>
             <Input textLabel="Пароль" placeholder="Введите пароль"/>
             <Button buttonName="Войти в аккаунт" buttonClass="account-btn"/>
-            <p>Ещё нет аккаунта?</p>
-            <Button buttonName="Зарегистрироваться"></Button>
+            <div>
+                <p>Ещё нет аккаунта?</p>
+                <Button buttonName="Зарегистрироваться"></Button>
+            </div>
         </form>
     )
 }
--- src/components/pages/loginPage.jsx
+++ src/components/pages/loginPage.jsx
@@ -10,9 +10,11 @@ export default function LoginPage(props) {
             <div className="loginPage-Container">
                 <div>
                     <PageTitle pageName="САМОЕ ВРЕМЯ ВЕРНУТЬСЯ К " pageNameItalic="ЗАНЯТИЯМ!"/>
-                    <p className="text-info">Cento – сервис для репетиторов и учеников по подготовке к ЕГЭ и ОГЭ. 
+                    <p className="text-info"><b>Cento</b> – сервис для репетиторов и учеников по подготовке к ЕГЭ и ОГЭ. 
                         Сюда бы ещё немного текста, но я пока не придумала какой, о думаю примерно столько будет в самый раз.
-                        <br></br>Занимайся с комфортом.</p>
+                        <br></br>
+                        <br></br>
+                        Занимайся с комфортом.</p>
                 </div>
                 <Form />
             </div>
