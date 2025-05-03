8dcebbb2c92326a373721b5b30b2480a9068615b
Emma Shfter <emmashifter04@gmail.com>
2025-05-04 00:14:51 +0500

    сделала попап с описанием конкретного урока

--- src/pages/FinanceReport.jsx
+++ src/pages/FinanceReport.jsx
@@ -52,7 +52,7 @@ export default function FinanceReport(props) {
             </div>
             <Footer />
 
-            <div className="popup-overlay">
+            <div className="hide popup-overlay">
                 <div className="popup finance-popup">
                     <div className="title-wrapper">
                         <p>Финансовый отчет</p>
@@ -64,6 +64,38 @@ export default function FinanceReport(props) {
                     <p className="date-select">Дата конца отчета</p>
                     <input className='date-select-input' type="date" id="start" name="trip-start" value="2018-01-01" min="2018-01-01" max="2018-12-31" />
                     <Button buttonClass="account-btn" buttonName="Сформировать отчет"></Button>
+                </div>
+            </div>
+
+            <div className="hide popup-overlay">
+                <div className="popup lesson-info-popup">
+                    <div className="title-wrapper">
+                        <p>Информация о занятии</p>
+                        <button className="cross-btn"></button>
+                    </div>
+                    <div className="lesson-info-popup-container">
+                        <div className="lesson-info-popup-subtitle-container">
+                            <p className="lesson-info-popup-subtitle">Ученик</p>
+                            <p>Иванов Иван Иванович</p>
+                        </div>
+                        <div className="lesson-info-popup-subtitle-container">
+                            <p className="lesson-info-popup-subtitle">Предмет</p>
+                            <p>Информатика</p>
+                        </div>
+                        <div className="lesson-info-popup-subtitle-container">
+                            <p className="lesson-info-popup-subtitle">Дата занятия</p>
+                            <p>12.05.2025 14:00</p>
+                        </div>
+                        <div className="lesson-info-popup-subtitle-container">
+                            <p className="lesson-info-popup-subtitle">Тема занятия</p>
+                            <p>Подготовка к ЕГЭ №13</p>
+                        </div>
+                    </div>
+                    <div className="popup-price-container">
+                        <p className="popup-price-title">Сумма к оплате:</p>
+                        <p className="popup-price-sum">1300 рублей</p>
+                    </div>
+                    <Button buttonClass="account-btn" buttonName="Оплатить"></Button>
                 </div>
             </div>
         </div>
